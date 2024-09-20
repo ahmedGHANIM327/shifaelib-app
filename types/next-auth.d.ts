f8c9571302e6e1dae49a6eea46d6e5c9351809db
ahmedGHANIM327 <ahmedghanim902@gmail.com>
2024-09-20 18:30:01 +0200

    fix lint errors

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -3,6 +3,7 @@ import NextAuth, { DefaultSession } from 'next-auth';
 declare module 'next-auth' {
   interface User {
     id: string;
+    cabinetId: string;
   }
   interface Session {
     user: User & {
